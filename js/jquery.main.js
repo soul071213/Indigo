241ddc879db9373359f38f81f2753e7a3869440f
soul071213 <js071213asd@gmail.com~>
2024-05-07 14:06:17 +0900

    0507

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.querySelector(".menu-toggle-btn");
-    const gnb =  document.querySelector(".gnb");
-    toggle.addEventListener("click", ()=>{
-        gnb.style.display = gnb.style.display === 'none' ? 'block':'none'; });
-}  );  
-jQuery(function($){
-    $(".menu-toggle-btn").click(function(){
-        $(".gnb").stop().slideToggle("fast");
-    })
-})
+    const gnb = document.querySelector(".gnb");
+    toggle.addEventListener("click", () => {
+      gnb.style.display = gnb.style.display === "none" ? "block" : "none";
+    });
+  });
+  
+  jQuery(function ($) {
+    $(".menu-toggle-btn").click(function () {
+      $(".gnb").stop().slideToggle("fast");
+    });
+  });
+  
